0e39d9a6f5adb1c0f99ee69d6b895efd506168c6
keiferms3 <keiferms3@gmail.com>
2024-09-04 18:47:26 -0400

    Gitignore db

--- database/models/artifacts.js
+++ database/models/artifacts.js
@@ -1,20 +1,23 @@
 export default (database, DataTypes) => {
     return database.define('artifacts', {
-       id: {
-           type: DataTypes.INTEGER,
-           primaryKey: true,
-       },
-       name: {
-           type: DataTypes.STRING,
-           allowNull: false,
-           unique: true,
-       },
-       emoji: {
-           type: DataTypes.STRING,
-           allowNull: false,
-       },
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        emoji: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        image: {
+            type: DataTypes.STRING
+        }
    }, {
-       timestamps: false,
+        timestamps: false,
    })
    }
    
