a4c5f013de8ec633e0d1a64039cc7cc5dd7453ea
keiferms3 <keiferms3@gmail.com>
2024-09-23 16:44:58 -0400

    Implemented inventory

--- commands/trinkets/inventory.js
+++ commands/trinkets/inventory.js
@@ -1,5 +1,5 @@
-import { SlashCommandBuilder } from "discord.js"
-import { Users } from "../../database/objects.js"
+import { EmbedBuilder, SlashCommandBuilder } from "discord.js"
+import { Config, Trinkets, Users } from "../../database/objects.js"
 
 export default {
     data: new SlashCommandBuilder()
@@ -15,19 +15,43 @@ export default {
     },
 }
 
+//TODO: Add multiple pages to inventory, add featured trinkets
 async function inventory(interaction) {
-    try {
+    try { 
         const user = interaction.options.getUser('user') ?? interaction.user
         const balance = await Users.getBalance(user.id, interaction.guild.id)
-        const embedColor = await Config.getConfig(interaction.guild.id, 'embedColor')
-        //Get trinket count here as well
-        if (balance == undefined) {
-            return `Balance not found for user "${user.displayName}"`
-        }
+        const config = await Config.getConfig(interaction.guild.id)
+        const trinkets = await Trinkets.getTrinkets(undefined, interaction.guild.id, user.id)
+        const tier1 = trinkets.filter(t => ( t.tier === 1 ))
+        const tier2 = trinkets.filter(t => ( t.tier === 2 ))
+        const tier3 = trinkets.filter(t => ( t.tier === 3 ))
         const embed = new EmbedBuilder()
-            .setColor(embedColor)
-            .setTitle(`${user.displayName}'s Balance`)
-            .setDescription(`:coin:  \`Placeholder Points\` | \`${balance} PP\`  :coin:\n:trophy:  \`Total Trinkets\` |  \`${0} T1\` \`${2} T2\` \`${1} T3\`  :trophy:`)
+            .setColor(config.embedColor)
+            .setTitle(`:package: ${user.displayName}'s Inventory :package:`)
+            
+        let desc = `:coin: \`${balance} PP\` :coin:\n\n`
+
+        if (tier3.length > 0) {
+            desc += `:first_place: **${config.rarityNameT3}** :first_place:\n`
+            for (const trinket of tier3) {
+                desc += `${trinket.emoji}\`${trinket.name}\` \`ID ${trinket.id}\`\n`
+            }
+        }
+        desc += '\n'
+        if (tier2.length > 0) {
+            desc += `:second_place: **${config.rarityNameT2}** :second_place:\n`
+            for (const trinket of tier2) {
+                desc += `${trinket.emoji}\`${trinket.name}\` \`ID ${trinket.id}\`\n`
+            }
+        }
+        desc += '\n'
+        if (tier1.length > 0) {
+            desc += `:third_place: **${config.rarityNameT1}** :third_place:\n`
+            for (const trinket of tier1) {
+                desc += `${trinket.emoji}\`${trinket.name}\` \`ID ${trinket.id}\`\n`
+            }
+        }
+        embed.setDescription(desc)
         
         return {embeds: [embed]}
     } catch (e) {
