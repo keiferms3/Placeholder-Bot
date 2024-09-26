3297132e3f7e02120da45e8ae7361008008580a7
keiferms3 <keiferms3@gmail.com>
2024-09-26 02:30:18 -0400

    Fixed multiple emojis on trinkets bug

--- commands/trinkets/trinket.js
+++ commands/trinkets/trinket.js
@@ -118,7 +118,7 @@ async function create(interaction, config) {
     const guildId = interaction.guild.id
     const tier = interaction.options.getInteger('rarity')
     const name = interaction.options.getString('name')
-    const emoji = interaction.options.getString('emoji')
+    const emojiString = interaction.options.getString('emoji')
     const image = interaction.options.getString('image')
     const lore = interaction.options.getString('description')
     const embed = new EmbedBuilder()
@@ -126,20 +126,21 @@ async function create(interaction, config) {
         .setTitle(`:x: Failed to create trinket :x:`)
     
     //Validation
-    if (!(emoji.match(emojiRegex()) || emoji.match(/<:.+?:\d+>$/))) { //This custom emote regex is a shitty solution, it doesn't stop emotes from other servers and matches with invalid emotes (eg <:slurslurslurs:1>)
+    if (!(emojiString.match(emojiRegex()) || emoji.match(/<:.+?:\d+>$/))) { //This custom emote regex is a shitty solution, it doesn't stop emotes from other servers and matches with invalid emotes (eg <:slurslurslurs:1>)
         return embed.setDescription(`Invalid emoji`)                  //There is a way to check if the bot can use the emote but, I'm too lazy and this is a private bot. Fuck you whoever inevitably makes me fix this :(   
     } else if (!(image === null || checkImageUrl(image))) {
         return embed.setDescription(`Invalid image URL. Make sure you are directly linking the image`)
     } //Character length limits
     else if (name.length > 64) { //Felt right
         return embed.setDescription(`Name has a character limit of 64. Your name was ${name.length} characters long`)
-    } else if (emoji.length > 64) { //Custom emojis should never be longer than 56 characters and this felt right
-        return embed.setDescription(`Emoji has a character limit of 64. Your "emoji" was ${emoji.length} characters long`)
+    } else if (emojiString.length > 64) { //Custom emojis should never be longer than 56 characters and this felt right
+        return embed.setDescription(`Emoji has a character limit of 64. Your "emoji" was ${emojiString.length} characters long`)
     } else if (lore && lore.length > 1200) { //Due to 2000 char message limit
         return embed.setDescription(`Description has a character limit of 1200. Your description was ${lore.length} characters long`)
     } else if (image && image.length > 512) { //1840 characters max user input, so we should never hit the 2000 limit if this info ever gets sent as a raw message
         return embed.setDescription(`Image URL has a character limit of 512. Your URL was ${image.length} characters long`)
     }
+    const emoji = emojiString.match(emojiRegex())[0]
 
     //Check balances, deduct points, and create trinket
     const user = await Users.getUser(interaction.user.id, guildId)
