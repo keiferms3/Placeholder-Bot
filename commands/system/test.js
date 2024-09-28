1513ca077c41e258848c55d48e48013b7e76a2e3
keiferms3 <keiferms3@gmail.com>
2024-09-27 20:40:04 -0400

    Removed /test forge reward script

--- commands/system/test.js
+++ commands/system/test.js
@@ -1,6 +1,4 @@
 import { SlashCommandBuilder } from "discord.js"
-import { forgeReward } from "../trinkets/trinket-gacha.js"
-import { Trinkets } from "../../database/objects.js"
 
 export default {
     data: new SlashCommandBuilder()
@@ -13,12 +11,5 @@ export default {
 }
 
 async function test(interaction) {
-    const trinkets = await Trinkets.getTrinkets(undefined, interaction.guild.id)
-    for (let trinket of trinkets) {
-        if (!trinket.ownerId.startsWith('gacha')) {
-            await forgeReward(trinket, interaction)
-        }
-    }
-    return 'FORGEMASTERS BE BLESSED!'
-    
+    return `What's up ${interaction.user.displayName}`
 }
