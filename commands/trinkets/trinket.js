81a3fad5e4f66962743e775621976f8af647e874
keiferms3 <keiferms3@gmail.com>
2024-09-27 23:42:55 -0400

    Added trinket returning, fixed trinket image validation

--- commands/trinkets/trinket.js
+++ commands/trinkets/trinket.js
@@ -95,6 +95,18 @@ export default {
                 visible
                 .setName('hidden'))
                 .setDescription('If true, command\'s output will not be visible to others'))
+        ))
+        //Trinket return subcommand
+        .addSubcommand((scrap) => (
+            scrap
+            .setName('return')
+            .setDescription('Return a trinket to the gacha for its creation value')
+            .addIntegerOption((int) => (
+                int
+                .setName('id')
+                .setDescription('Trinket\'s unique ID number')
+                .setRequired(true)
+            ))
         )),
     async execute(interaction) {
         try {
@@ -107,20 +119,21 @@ export default {
 }
 
 async function trinket(interaction) {
-    const config = await Config.getConfig(interaction.guild.id)
-    
     const command = interaction.options.getSubcommand()
     if (command === 'create') {
-        var response = await create(interaction, config)
+        var response = await create(interaction)
     } 
     else if (command === 'roll') {
         var response = await displayGacha(interaction)
     } 
     else if (command === 'view') {
-        var response = await view(interaction, config)
+        var response = await view(interaction)
     } 
     else if (command === 'search') {
-        var response = await search(interaction, config)
+        var response = await search(interaction)
+    } 
+    else if (command === 'return') {
+        var response = await scrap(interaction)
     } 
     else {
         return `Tinket command "\`${command}\`" not found`
@@ -129,58 +142,48 @@ async function trinket(interaction) {
     return response
 }
 
-async function create(interaction, config) {
+async function create(interaction) {
+    const config = await Config.getConfig(interaction.guild.id)
     const guildId = interaction.guild.id
     const tier = interaction.options.getInteger('rarity')
     const name = interaction.options.getString('name')
     const emojiString = interaction.options.getString('emoji')
     const image = interaction.options.getString('image')
     const lore = interaction.options.getString('description')
-    const ephemeral = interaction.options.getBoolean('hidden')
+    let ephemeral = interaction.options.getBoolean('hidden')
     const embed = new EmbedBuilder()
         .setColor(config.embedColor)
         .setTitle(`:x: Failed to create trinket :x:`)
-    
+
     //Validation
+    if (image !== null) { //If image exists
+        const isValidImage = await checkImageUrl(image) //Await validation
+        if (!isValidImage) {
+            return {embeds: [embed.setDescription(`Invalid image URL. Make sure you are directly linking the image`)]}
+        }
+    }
     if (!(emojiString.match(emojiRegex()) || emojiString.match(/<:.+?:\d+>$/))) { //This custom emote regex is a shitty solution, it doesn't stop emotes from other servers and matches with invalid emotes (eg <:slurslurslurs:1>)
         return embed.setDescription(`Invalid emoji`)                  //There is a way to check if the bot can use the emote but, I'm too lazy and this is a private bot. Fuck you whoever inevitably makes me fix this :(   
-    } else if (!(image === null || checkImageUrl(image))) {
-        return embed.setDescription(`Invalid image URL. Make sure you are directly linking the image`)
-    } //Character length limits
-    else if (name.length > 64) { //Felt right
+    } 
+    const emoji = emojiString.match(emojiRegex()) ? emojiString.match(emojiRegex())[0] : emojiString
+    //Character length limits
+    if (name.length > 64) { //Felt right
         return embed.setDescription(`Name has a character limit of 64. Your name was ${name.length} characters long`)
-    } else if (emojiString.length > 64) { //Custom emojis should never be longer than 56 characters and this felt right
+    } else if (emoji.length > 64) { //Custom emojis should never be longer than 56 characters and this felt right
         return embed.setDescription(`Emoji has a character limit of 64. Your "emoji" was ${emojiString.length} characters long`)
     } else if (lore && lore.length > 1200) { //Due to 2000 char message limit
         return embed.setDescription(`Description has a character limit of 1200. Your description was ${lore.length} characters long`)
     } else if (image && image.length > 512) { //1840 characters max user input, so we should never hit the 2000 limit if this info ever gets sent as a raw message
         return embed.setDescription(`Image URL has a character limit of 512. Your URL was ${image.length} characters long`)
     }
-    const emoji = emojiString.match(emojiRegex()) ? emojiString.match(emojiRegex())[0] : emojiString
 
     //Check balances, deduct points, and create trinket
     const user = await Users.getUser(interaction.user.id, guildId)
-    if (tier === 1) {
-        if (user.points < config.trinketCostT1) {
-            embed.setDescription(`${config.rarityNameT1} trinkets require \`${config.trinketCostT1} PP\`, you have \`${user.points} PP\``)
-            return {embeds: [embed]}
-        }
-        await Users.updateBalance(user.userId, user.guildId, -1*config.trinketCostT1)
+    if (user.points < config[`trinketCostT${tier}`]) {
+        embed.setDescription(`${config[`rarityNameT${tier}`]} trinkets require \`${config[`trinketCostT${tier}`]} PP\`, you have \`${user.points} PP\``)
+        return {embeds: [embed]}
     }
-    else if (tier === 2) {
-        if (user.points < config.trinketCostT2) {
-            embed.setDescription(`${config.rarityNameT2} trinkets require \`${config.trinketCostT2} PP\`, you have \`${user.points} PP\``)
-            return {embeds: [embed]}
-        }
-        await Users.updateBalance(user.userId, user.guildId, -1*config.trinketCostT2)
-    }
-    else if (tier === 3) {
-        if (user.points < config.trinketCostT3) {
-            embed.setDescription(`${config.rarityNameT3} trinkets require \`${config.trinketCostT3} PP\`, you have \`${user.points} PP\``)
-            return {embeds: [embed]}
-        }
-        await Users.updateBalance(user.userId, user.guildId, -1*config.trinketCostT3)
-    }
+    await Users.updateBalance(user.userId, user.guildId, -1*config[`trinketCostT${tier}`])
 
     const trinket = await Trinkets.addTrinket(tier, name, emoji, image, lore, user.userId, guildId, ephemeral, interaction)
     UpdateGachaChance(tier, interaction)
@@ -191,9 +194,10 @@ async function create(interaction, config) {
     return {embeds: [embed], ephemeral: ephemeral}
 }
 
-async function view(interaction, config) {
+async function view(interaction) {
+    const config = await Config.getConfig(interaction.guild.id)
     const id = interaction.options.getInteger('id')
-    const ephemeral = interaction.options.getBoolean('hidden')
+    let ephemeral = interaction.options.getBoolean('hidden')
 
     let embed
     const trinket = await Trinkets.getTrinkets(id)
@@ -205,13 +209,38 @@ async function view(interaction, config) {
         }
         
     } else {
-        embed = new EmbedBuilder().setTitle('oops')
+        embed = new EmbedBuilder().setTitle(`:x: Trinket \`ID ${id}\` doesn't exist :x:`)
+        ephemeral = true
     }
     return {embeds: [embed], ephemeral: ephemeral}
 }
 
-async function search(interaction, config) {
+async function search(interaction) {
     
+}
+
+async function scrap(interaction) {
+    const config = await Config.getConfig(interaction.guild.id)
+    const id = interaction.options.getInteger('id')
+    let ephemeral = false
+
+    const embed = new EmbedBuilder()
+        .setColor(config.embedColor)
+    const trinket = await Trinkets.getTrinkets(id)
+    if (trinket && trinket.ownerId === interaction.user.id) {
+        trinket.ownerId = `gacha${trinket.tier}`
+        trinket.save()
+        embed.setTitle(`:white_check_mark: ${interaction.user.displayName} returned ${config[`rarityNameT${trinket.tier}`]} ${trinket.emoji}\`${trinket.name}\` \`(ID ${trinket.id})\` to the gacha! :white_check_mark:`)
+             .setDescription(`\`${config[`trinketCostT${trinket.tier}`]} PP\` added to balance`)
+        Users.updateBalance(interaction.user.id, interaction.guild.id, config[`trinketCostT${trinket.tier}`])
+    } else if (trinket) {
+        embed.setTitle(`:x: You don't own trinket \`ID ${id}\` :x:`)
+        ephemeral = true
+    } else {
+        embed.setTitle(`:x: Trinket \`ID ${id}\` doesn't exist :x:`)
+        ephemeral = true
+    }
+    return {embeds: [embed], ephemeral: ephemeral}
 }
 
 async function display(trinket, interaction, config, hidden = false) {
@@ -239,6 +268,7 @@ async function display(trinket, interaction, config, hidden = false) {
     return embed
 }
 
+//Returns true if url is a direct image link
 async function checkImageUrl(url) {
     const res = await fetch(url);
     const buff = await res.blob();
