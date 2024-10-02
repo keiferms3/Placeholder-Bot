d362533f7b87ebd888e956abd0a37b10f3da01d9
keiferms3 <keiferms3@gmail.com>
2024-10-02 13:43:17 -0400

    Return now rounds value

--- commands/trinkets/trinket.js
+++ commands/trinkets/trinket.js
@@ -303,8 +303,8 @@ async function trinketReturn(interaction) {
             trinket.returned = true
             trinket.save()
             embed.setTitle(`:white_check_mark: ${interaction.user.displayName} returned ${config[`rarityNameT${trinket.tier}`]} ${trinket.emoji}\`${trinket.name}\` \`(ID ${trinket.id})\` to the gacha! :white_check_mark:`)
-                 .setDescription(`\`${config[`trinketCostT${trinket.tier}`]*config[`returnRatioT${trinket.tier}`]} PP\` added to balance`)
-                 Users.updateBalance(interaction.user.id, interaction.guild.id, (config[`trinketCostT${trinket.tier}`]*config[`returnRatioT${trinket.tier}`]))
+                 .setDescription(`\`${Math.round(config[`trinketCostT${trinket.tier}`]*config[`returnRatioT${trinket.tier}`])} PP\` added to balance`)
+                 Users.updateBalance(interaction.user.id, interaction.guild.id, (Math.round(config[`trinketCostT${trinket.tier}`]*config[`returnRatioT${trinket.tier}`])))
             
             await interaction.followUp({embeds: [embed], components: []})
         } else if (buttonId === 'returnCancel') {
